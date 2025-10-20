9f1ca0be02a69633e099ee8d5c25ceff013c5c7b
shenjingnan <sjn.code@gmail.com>
2025-10-20 19:28:58 +0800

    WIP

--- packages/bestmcp/src/core/transports/index.ts
+++ packages/bestmcp/src/core/transports/index.ts
@@ -1,4 +1,3 @@
-// 传输层相关导出
 export { BaseTransport, type HTTPTransportConfig, type TransportConfig, TransportType } from "./base.js";
 export { HTTPTransport } from "./http.js";
 export { StdioTransport } from "./stdio.js";
