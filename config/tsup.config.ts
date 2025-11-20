b465e385934320368f8435aee04ffd951ab8abe9
shenjingnan <sjn.code@gmail.com>
2025-11-20 18:00:48 +0800

    WIP

--- config/tsup.config.ts
+++ config/tsup.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
@@ -14,13 +15,13 @@ export default defineConfig({
   target: "node18",
   external: [],
   tsconfig: "tsconfig.json",
-  // 使用tsup内置的alias支持
+  // 使用tsup内置的alias支持，采用绝对路径确保一致性
   alias: {
-    "@": "./src",
-    "@/types": "./src/types",
-    "@/utils": "./src/utils",
-    "@/services": "./src/services",
-    "@/mocks": "./src/mocks",
-    "@/test": "./src/test",
+    "@": path.resolve(__dirname, "../src"),
+    "@/types": path.resolve(__dirname, "../src/types"),
+    "@/utils": path.resolve(__dirname, "../src/utils"),
+    "@/services": path.resolve(__dirname, "../src/services"),
+    "@/mocks": path.resolve(__dirname, "../src/mocks"),
+    "@/test": path.resolve(__dirname, "../src/test"),
   },
 });
